90788cc98bc9a64c01f1ca260a49d623aa3c169d
Noah Feder <noah.feder@gmail.com>
2016-08-21 18:47:36 -0400

    add phantomjs cloud API route

--- server.rb
+++ server.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-require 'screencap'
 
 get "/" do
   erb :index
@@ -12,12 +11,7 @@ end
 get "/:top/:bottom/print" do
   @top = params[:top]
   @bottom = params[:bottom]
-  f = Screencap::Fetcher.new('http://localhost:9292/#{@top}/#{@bottom}')
-  screenshot = f.fetch(
-    :output => '/cap.png',
-    :div => '.wrapper'
-    )
-  screenshot
+  redirect to("https://phantomjscloud.com/api/browser/v2/ak-bgqmn-rfvvx-gjhyt-2y25g-v17k3/?request={url:%22https://st-text.herokuapp.com/#{@top}/#{@bottom}%22,renderType:%22png%22,requestSettings:{waitInterval:4000},renderSettings:{viewport:{height:350,width:750},clipRectangle:{height:230,width:750,top:0,left:0}}}")
 end
 get "/:garbage" do
   erb :index
