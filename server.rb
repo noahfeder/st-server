6377caa4599bc651ecf861c3ab875b795ab3450d
Noah Feder <noah.feder@gmail.com>
2016-08-23 13:42:39 -0400

    dynamic line length for static view

--- server.rb
+++ server.rb
@@ -1,16 +1,34 @@
 require 'sinatra'
 require 'dotenv'
-
+require 'pry'
 Dotenv.load
+
+letters = {a: 84, b: 76, c: 77, d: 82, e: 71, f: 65,
+  g: 86, h: 80, i: 35, j: 67, k: 85, l: 65, m: 87, n: 72, o: 82, p: 72,
+  q: 87, r: 83, s: 67, t: 70, u: 76, v: 77, w: 107, x: 82, y: 78, z: 71}
 
 get "/" do
   erb :index
 end
-get "/:top/:bottom" do
-  @top = params[:top]
+get "/:top/:bottom/static" do
+  @top = params[:top] #stranger
   @bottom = params[:bottom]
-  @top_length = @top.length * 85
-  @bottom_length = @bottom.length * 72
+  @top_length = 0
+  @bottom_length = 0
+  @top.split('').each_with_index do |letter,index|
+    if (index == 0 || index == (@top.length - 1))
+      @top_length += (letters[letter.to_sym] * 1.3)
+    else
+      @top_length += letters[letter.to_sym]
+    end
+  end
+  @bottom.split('').each_with_index do |letter,index|
+    if (index == 0 || index == (@bottom.length - 1))
+      @bottom_length += (letters[letter.to_sym] * 1.3)
+    else
+      @bottom_length += letters[letter.to_sym]
+    end
+  end
   def is_first_longer?
     @top_length > @bottom_length
   end
@@ -42,7 +60,7 @@ end
 get "/:top/:bottom/print" do
   @top = params[:top]
   @bottom = params[:bottom]
-  redirect to("https://phantomjscloud.com/api/browser/v2/#{ENV['KEY']}/?request={url:%22https://st-text.herokuapp.com/#{@top}/#{@bottom}%22,renderType:%22png%22,renderSettings:{viewport:{height:400,width:750},clipRectangle:{height:400,width:750,top:0,left:0}}}")
+  redirect to("https://phantomjscloud.com/api/browser/v2/#{ENV['KEY']}/?request={url:%22https://st-text.herokuapp.com/#{@top}/#{@bottom}/static%22,renderType:%22png%22,renderSettings:{viewport:{height:400,width:750},clipRectangle:{height:400,width:750,top:0,left:0}}}")
 end
 get "/:garbage" do
   erb :index
