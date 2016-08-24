5fa4eb92087823d66bfae5b809a93cb9ac44dcc2
Noah Feder <noah.feder@gmail.com>
2016-08-24 01:03:56 -0400

    fixing screwup on print

--- server.rb
+++ server.rb
@@ -60,7 +60,7 @@ end
 get "/:top/:bottom/print" do
   @top = params[:top]
   @bottom = params[:bottom]
-  "https://phantomjscloud.com/api/browser/v2/#{ENV['KEY']}/?request={url:%22https://st-text.herokuapp.com/#{@top}/#{@bottom}/static%22,renderType:%22png%22,renderSettings:{viewport:{height:400,width:750},clipRectangle:{height:400,width:750,top:0,left:0}}}"
+  redirect to("https://phantomjscloud.com/api/browser/v2/#{ENV['KEY']}/?request={url:%22https://st-text.herokuapp.com/#{@top}/#{@bottom}/static%22,renderType:%22png%22,renderSettings:{viewport:{height:400,width:750},clipRectangle:{height:400,width:750,top:0,left:0}}}")
 end
 get "/:top/:bottom" do
   @top = params[:top].downcase.gsub(/[^a-z]/i, "")
