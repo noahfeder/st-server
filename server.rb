4475246603e55cd09e904358b4ac8d4cc691c15f
Noah Feder <noah.feder@gmail.com>
2016-10-26 23:01:14 -0400

    whitespace

--- server.rb
+++ server.rb
@@ -15,6 +15,7 @@ letters = {a: 84, b: 76, c: 77, d: 82, e: 71, f: 65,
 get "/" do
   erb :index
 end
+
 get "/:top/:bottom/static" do
   @top = params[:top].downcase.gsub(/[^a-z]/i, "")[0..7]
   @bottom = params[:bottom].downcase.gsub(/[^a-z]/i, "")[0..7]
@@ -66,17 +67,19 @@ get "/:top/:bottom/static" do
   @middle = @top[1..(@top.length - 2)]
   erb :static
 end
+
 get "/:top/:bottom/print" do
   @top = params[:top]
   @bottom = params[:bottom]
   redirect to("https://phantomjscloud.com/api/browser/v2/#{ENV['KEY']}/?request={url:%22https://st-text.herokuapp.com/#{@top}/#{@bottom}/static%22,renderType:%22png%22,renderSettings:{viewport:{height:400,width:750},clipRectangle:{height:400,width:750,top:0,left:0}}}")
 end
+
 get "/:top/:bottom" do
   @top = params[:top].downcase.gsub(/[^a-z]/i, "")[0..7]
   @bottom = params[:bottom].downcase.gsub(/[^a-z]/i, "")[0..7]
   erb :index
 end
+
 get "/:garbage" do
   erb :index
 end
-
